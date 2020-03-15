cad7c366c1c55578018f704bcbdf5b4c26621fba
hiroki.daitoku <hirokidaitoku@gmail.com>
2020-03-15 01:35:01 +0000

    amend

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,13 +5,13 @@ class FavoritesController < ApplicationController
     micropost = Micropost.find(params[:micropost_id])
     current_user.favorite(micropost)
     flash[:success] = "投稿をお気に入りしました。"
-    redirect_to root_url
+    redirect_to likes_user_path(current_user)
   end
 
   def destroy
     micropost = Micropost.find(params[:micropost_id])
     current_user.unfavorite(micropost)
     flash[:danger] = "お気に入りを解除しました。"
-    redirect_to root_url
+    redirect_to likes_user_path(current_user)
   end
 end
